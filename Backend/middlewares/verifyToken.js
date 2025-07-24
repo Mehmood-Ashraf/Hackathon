11db7541407655d465f01ab2845726ba155ef9dc
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2025-07-25 01:42:51 +0500

    "Fix error"

--- Backend/middlewares/verifyToken.js
+++ Backend/middlewares/verifyToken.js
@@ -5,11 +5,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const verifyToken = (req, res, next) => {
-  const token = req?.cookies?.access_token || req.headers("Authorization")?.replace("Bearer ", "")
+  const token = req?.cookies?.access_token || req.headers["Authorization"]?.replace("Bearer ", "")
   console.log(req.header)
   if (!token) {
-    return errorHandler(res, 401, "You are not authenticated!");
     console.log("Not token")
+    return errorHandler(res, 401, "You are not authenticated!");
   }
   try {
     const verifiedUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
